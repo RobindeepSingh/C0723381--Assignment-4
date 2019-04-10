7844918970abcfdaa89ae9b8ef25a19fb74f4d44
visualstudio <visualstudio@122-PC37>
2019-04-10 08:37:29 -0400

    8:37 commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,26 @@ namespace ConsoleApp1
             Program p = new Program();
             p.Beowulf = new ArrayList();
         }
+        public void Run()
+        {
+            this.ReadTextFiles();
+        }
+
+        public void ReadTextFiles()
+        {
+            using (StreamReader file = new StreamReader (@"U:\Users\723381\beowulf.txt"))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine())!= null)
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+                }
+
+            }
+        }
     }
-    public void Run() { this.ReadTextFiles(); }
+   
 }
